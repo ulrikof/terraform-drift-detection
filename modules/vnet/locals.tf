02deafd168fe6055faf26f65414c9cea71291850
ulrikof <ulrikof@stud.ntnu.no>
2024-11-25 13:15:36 +0100

    fmt

--- modules/vnet/locals.tf
+++ modules/vnet/locals.tf
@@ -1,6 +1,6 @@
-locals {  
+locals {
   prefix = "uo_"
   suffix = "_01"
 
-  vnet_name  = "${local.prefix}${var.vnet_base_name}${local.suffix}"
+  vnet_name = "${local.prefix}${var.vnet_base_name}${local.suffix}"
 }
